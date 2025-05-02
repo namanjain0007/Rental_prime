c03a9d247a2895b1bc4f7969fe993e0a12cca09d
namanjain <namanjainayushjain7@gmail.com>
2025-05-02 13:21:36 +0530

    comit

--- backend/database/postgres.js
+++ backend/database/postgres.js
@@ -2,16 +2,19 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render external DB connection
+  },
 });
 
 pool
   .connect()
-  .then(() => console.log("PostgreSQL connected!"))
-  .catch((err) => console.error("Connection error", err.stack));
+  .then(() => console.log("PostgreSQL connected!")) // Replace with your preferred logging method
+  .catch((err) => console.error("Connection error", err.stack)); // Replace with your preferred logging method
 
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -47,5 +47,5 @@ app.use("/notification", notificationRoutes);
 //Server listening on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at ${process.env.PG_HOST}:${PORT}`);
+  console.log(`Server running at ${process.env.DB_HOST}:${PORT}`);
 });
