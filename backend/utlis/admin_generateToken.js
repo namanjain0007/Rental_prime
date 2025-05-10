7e28e209536d60d5e79b3e339eb424aba79f0277
namanjain <namanjainayushjain7@gmail.com>
2025-05-10 16:30:59 +0530

    add email and name to admin jwt token

--- backend/utlis/admin_generateToken.js
+++ backend/utlis/admin_generateToken.js
@@ -5,6 +5,8 @@ const admin_generateToken = (admin) => {
     {
       admin_id: admin.admin_id,
       admin_user_type: admin.admin_user_type,
+      email: admin.email,
+      name: admin.name,
     },
     process.env.JWT_SECRET,
     { expiresIn: process.env.JWT_EXPIRES_IN }
